00fbf99d79e7f85668dfb984f526dbcb169a6f4b
ziyacaylan <ziyacaylan@gmail.com>
2023-01-30 18:28:46 +0300

    work on colors

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Tailwind Css Tutorial
 
 ---
+
+## Project Skeleton
+
+|----README.md
+├── public
+│ ├── css
+│ │ └── styles.css
+│ ├── images
+│ └── lindex.html
+├── .gitignore
+├── input.css
+├── package-lock.json
+├── package.json
+└── tailwind.config.js
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ["Gemunu Libre", "sans-serif"],
+        open: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        "light-red": "#ef4444",
+        "dark-orange": "#ea580c",
+        "light-yellow": "#fde68a",
+        "light-green": "#65a30d",
+        "dark-cyan": "#0891b2",
+      },
+    },
   },
   plugins: [],
 };
